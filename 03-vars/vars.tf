5f1798b02042f6d42ccf3c84a799aa28d5af70f2
gaddamrk <raj>
2023-07-12 11:57:43 +0530

    jr

--- 03-vars/vars.tf
+++ 03-vars/vars.tf
@@ -5,3 +5,19 @@ variable "sample" {
 output "sample" {
   value = var.sample
 }
+
+# string data type
+
+variable "sample1" {
+  default = "hello world"
+}
+
+# number data type
+variable "sample2" {
+  default = 100
+}
+
+#boolean data type
+variable "data"  {
+  default = true
+}
