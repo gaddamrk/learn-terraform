5844e87006bcf8928e0c7cbff03b0c15dc05e50c
gaddamrk <raj>
2023-07-19 09:18:06 +0530

    jr

--- 11-provisioners/main.tf
+++ 11-provisioners/main.tf
@@ -30,19 +30,6 @@ resource "aws_instance" "web" {
 }
 
 
-
-
-
-
-resource "aws_instance" "web" {
-  ami                     = data.aws_ami.centos8.id
-  instance_type           = "t3.micro"
-  vpc_security_group_ids  = [aws_security_group.allow_tls.id]
-
-  tags = {
-    Name = "test-centos8"
-  }
-}
 resource "aws_security_group" "allow_tls" {
   name        = "allow_tls"
   description = "Allow TLS inbound traffic"
